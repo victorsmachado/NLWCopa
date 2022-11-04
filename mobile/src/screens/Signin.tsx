1df3064e0a4304214029c11a5bb865e976ed520b
Victor Machado <48452755+victorsmachado@users.noreply.github.com>
2022-11-04 16:19:11 -0300

    Add files via upload

---
+++ mobile/src/screens/Signin.tsx
@@ -0,0 +1,34 @@
+import { Center, Text, Icon } from "native-base";
+import { Fontisto } from "@expo/vector-icons";
+
+import { useAuth } from "../hooks/useAuth";
+
+import Logo from "../assets/logo.svg";
+import { Button } from "../components/Button";
+
+export function SignIn() {
+  const { signIn, isUserLoading } = useAuth();
+
+  return (
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40} />
+
+      <Button
+        title="ENTRAR COM GOOGLE"
+        type="SECONDARY"
+        leftIcon={
+          <Icon as={Fontisto} name="google" color="white" size={"md"} />
+        }
+        mt={12}
+        onPress={signIn}
+        isLoading={isUserLoading}
+        _loading={{ _spinner: { color: "white" } }}
+      />
+
+      <Text color={"white"} textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {"\n"} do seu nome e e-mail para
+        criação da sua conta.
+      </Text>
+    </Center>
+  );
+}
---
+++ server/src/server.ts
@@ -0,0 +1,35 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
+
+import { poolRoutes } from "./routes/pool";
+import { gameRoutes } from "./routes/game";
+import { userRoutes } from "./routes/user";
+import { authRoutes } from "./routes/auth";
+import { guessRoutes } from "./routes/guess";
+
+async function bootstrap() {
+  const fastify = Fastify({
+    logger: true,
+  });
+
+  await fastify.register(cors, {
+    origin: true,
+  });
+
+  //Em produção isso precisa ser uma váriavel ambiente!!!!!
+
+  await fastify.register(jwt, {
+    secret: "nlwcopa",
+  });
+
+  await fastify.register(poolRoutes);
+  await fastify.register(authRoutes);
+  await fastify.register(gameRoutes);
+  await fastify.register(userRoutes);
+  await fastify.register(guessRoutes);
+
+  await fastify.listen({ port: 3333, host: "0.0.0.0" });
+}
+
+bootstrap();
